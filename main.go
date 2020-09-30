02829e438c08d224868b2417e9670743a475d92d
Andrew Thielking <athielking@gmail.com>
2020-09-30 06:11:54 -0400

    made a aws lambda function

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+//MyEvent is ... Lambda Function Input Object
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func handleRequest(ctx context.Context, httpEvent events.APIGatewayProxyRequest) (string, error) {
+	bytes := []byte(httpEvent.Body)
+	var evt MyEvent
+	err := json.Unmarshal(bytes, &evt)
+
+	if err != nil {
+		return "ERROR", err
+	}
+
+	return fmt.Sprintf("Hello %s", evt.Name), nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
